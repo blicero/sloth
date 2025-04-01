739b24db5a21e4ba6352e2bc991337b183900266
Benjamin Walkenhorst <krylon@posteo.de>
2025-04-01 21:47:45 +0200

    Add minimal skeleton for interactive shell interface

---
+++ shell.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2025-04-01 19:59:37 krylon>
+#
+# /data/code/python/sloth/shell.py
+# created on 01. 04. 2025
+# (c) 2025 Benjamin Walkenhorst
+#
+# This file is part of the Sloth Meta Package Manager. It is distributed under the
+# terms of the GNU General Public License 3. See the file LICENSE for details
+# or find a copy online at https://www.gnu.org/licenses/gpl-3.0
+
+"""
+sloth.shell
+
+(c) 2025 Benjamin Walkenhorst
+"""
+
+from cmd import Cmd
+
+
+class Shell(Cmd):
+    """Interactive interface to the package manager."""
+
+
+# Local Variables: #
+# python-indent: 4 #
+# End: #
